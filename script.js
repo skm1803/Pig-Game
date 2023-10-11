a0de5be123f72e18ca9ecca5138950c0144edbe4
Shubham Kr. Mishra <shubham.mishrakiit@gmail.com>
2023-10-11 09:20:09 +0530

    Displyed random dice value alos add dice to current score other then 1

--- script.js
+++ script.js
@@ -3,7 +3,28 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnRollEl = document.querySelector('.btn--roll');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+let currentScore =0;
 
 score0El.textContent=0;
 score1El.textContent=0;
 diceEl.classList.add('hidden');
+
+btnRollEl.addEventListener('click',function(){
+    const dice =Math.trunc((Math.random()*6)+1);
+    console.log(dice);
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    if( dice !==1){
+        currentScore += dice;
+        current0El.textContent= currentScore;
+    }
+    else{
+        //Switch to next player
+    }
+})
+
